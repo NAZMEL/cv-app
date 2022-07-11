0ddf9677bb7f310e4881c5e0d65112430dedc1c6
NAZMEL <melnyknasar@gmail.com>
2022-07-11 10:48:54 +0300

    Add styles for education section

--- client/src/components/Education/Education.jsx
+++ client/src/components/Education/Education.jsx
@@ -4,11 +4,13 @@ import style from "./Education.module.css";
 const Education = ({ education }) => {
   return (
     <div className={style.educationSection}>
-      Education
+      <div className={style.educationTitle}>Education and courses</div>
+
       {education.map((item) => {
         return (
           <div key={item.area} className={style.educationBlock}>
             <span className={style.educationArea}>{item.area}</span>
+            <hr/>
             <span className={style.educationPlace}>{item.place}</span>
             <span className={style.eduactionDate}>{item.dates}</span>
           </div>
