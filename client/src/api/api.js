d880ba2be6527457d12c8a7c969f21e303f17ad1
NAZMEL <melnyknasar@gmail.com>
2022-07-12 15:54:43 +0300

    set proxy on the server

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    //withCredentials: true,
     baseURL: 'http://localhost:5000/api/',
-    //baseURL: 'https://social-network.samuraijs.com/api/1.0/'
 })
 
 export default instance;
